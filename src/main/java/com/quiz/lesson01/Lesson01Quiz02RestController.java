7210f2144fddcb322c2a27cf18b04e70490aa31a
user <user>
2022-09-20 19:48:23 +0900

    초기 환경 세팅

---
+++ src/main/java/com/quiz/lesson01/Lesson01Quiz02RestController.java
@@ -0,0 +1,99 @@
+package com.quiz.lesson01;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // @Controller + @ResponseBody
+@RequestMapping("/lesson01/quiz02")
+public class Lesson01Quiz02RestController {
+
+	// 요청 URL : http://localhost:8080/lesson01/quiz02/1
+	@RequestMapping("/1")
+	public List<Map<String, Object>> quiz01_1() {
+		// @ResponseBody + return String => HttpMessageConverter String => 글자가 response body에 담긴다
+		// @ResponseBody + return 객체 => HttpMessageConverter 객체 jackson 라이브러리 => JSON 응답 - API
+		
+		List<Map<String, Object>> list = new ArrayList<>();
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("rate", 15);
+		map.put("director", "봉준호");
+		map.put("time", 131);
+		map.put("title", "기생충");
+		list.add(map);
+		
+		map = new HashMap<>();
+		map.put("rate", 0);
+		map.put("director", "로베르토 베니니");
+		map.put("time", 116);
+		map.put("title", "인생은 아름다워");
+		list.add(map);
+		
+		map = new HashMap<>();
+		map.put("rate", 12);
+		map.put("director", "크리스토퍼 놀란");
+		map.put("time", 147);
+		map.put("title", "인셉션");
+		list.add(map);
+
+		map = new HashMap<>();
+		map.put("rate",19 );
+		map.put("director", "윤종빈");
+		map.put("time", 133);
+		map.put("title", "범죄와의 전쟁 : 나쁜놈들 전성시대");
+		list.add(map);
+
+		map = new HashMap<>();
+		map.put("rate", 15);
+		map.put("director", "프란시스 로렌스");
+		map.put("time", 137);
+		map.put("title", "헝거게임");
+		list.add(map);
+		
+		return list;
+	}
+	
+	// 요청 URL : http://localhost:8080/lesson01/quiz02/2
+	@RequestMapping("/2")
+	public List<Data> quiz02_2() {
+		List<Data> list = new ArrayList<>();
+		
+		Data data = new Data(); // 일반 java bean
+		data.setTitle("안녕하세요 가입인사 드립니다");
+		data.setUser("hagulu");
+		data.setContent("안녕하세요. 가입했어요. 앞으로 잘 부탁드립니다. 활동 열심히 하겠습니다.");
+		list.add(data);
+		
+		data = new Data();
+		data.setTitle("헐 대박");
+		data.setUser("bada");
+		data.setContent("오늘 목요일 이었어... 금요일인줄");
+		list.add(data);
+		
+		data = new Data();
+		data.setTitle("오늘 데이트 한 이야기 해드릴게요.");
+		data.setUser("dulumary");
+		data.setContent("....");
+		list.add(data);
+		
+		return list;
+	}
+	
+	// 요청 URL : http://localhost:8080/lesson01/quiz02/3
+	@RequestMapping("/3")
+	public ResponseEntity<Data> quiz02_3() {
+		Data data = new Data();
+		data.setTitle("안녕하세요 가입인사 드립니다");
+		data.setUser("hagulu");
+		data.setContent("안녕하세요. 가입했어요. 앞으로 잘 부탁드립니다. 활동 열심히 하겠습니다.");
+
+		return new ResponseEntity<>(data, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
