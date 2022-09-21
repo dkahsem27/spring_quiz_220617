ed321a6c61f8241fa164804699facb42e90ebc22
user <user>
2022-09-21 20:45:55 +0900

    0920 과제 - MyBatis DB연동 및 테이블 내용 json 출력(수정)

--- src/main/java/com/quiz/lesson02/bo/StoreBO.java
+++ src/main/java/com/quiz/lesson02/bo/StoreBO.java
@@ -2,19 +2,22 @@ package com.quiz.lesson02.bo;
 
 import java.util.List;
 
-import org.apache.catalina.Store;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.quiz.lesson02.dao.StoreDAO;
+import com.quiz.lesson02.model.Store;
 
 @Service
 public class StoreBO {
 	
+	// DI(Dependency Injection): 의존성 주입
 	@Autowired
 	private StoreDAO storeDAO;
 	
+	// input:X,   output:List<Store>
 	public List<Store> getStoreList() {
-		return storeDAO.selectStoreList();
+		List<Store> storeList = storeDAO.selectStoreList();
+		return storeList;
 	}
 }
