01af48f3f74d641bcc53ad7f77e4db0666c66d2e
user <user>
2022-09-22 21:42:42 +0900

    0922 select문 다루기

---
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -0,0 +1,28 @@
+package com.quiz.lesson03.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson03.dao.RealEstateDAO;
+import com.quiz.lesson03.model.RealEstate;
+
+@Service
+public class RealEstateBO {
+
+	@Autowired
+	private RealEstateDAO realEstateDAO;
+	
+	public RealEstate getRealEstateById(int id) {
+		return realEstateDAO.selectRealEstateById(id);
+	}
+	
+	public List<RealEstate> getRealEstateListByRentPrice(int rentPrice) {
+		return realEstateDAO.selectRealEstateListByRentPrice(rentPrice);
+	}
+	
+	public List<RealEstate> getRealEstateListByAreaAndPrice(int area, int price) {
+		return realEstateDAO.selectRealEstateListByAreaAndPrice(area, price);
+	}
+}
